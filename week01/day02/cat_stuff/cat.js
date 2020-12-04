dc846a2b9435f89e40803b479d8f01808fd27364
Nathan-at-work <nmp14fsu@gmail.com>
2020-12-04 13:36:20 -0500

    Added section for changing text color of 'Weezy Is The Best. JS not added yet

--- week01/day02/cat_stuff/cat.js
+++ week01/day02/cat_stuff/cat.js
@@ -1,6 +1,8 @@
+// Onclick actions for growing and shrinking image. #img1
 document.getElementById("grow").onclick = function () { growImg() };
 document.getElementById("shrink").onclick = function () { shrinkImg() };
 
+//Functions
 growImg = () => {
     document.getElementById("img1").style.width = '350px';
 }
